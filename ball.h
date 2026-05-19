8aca78effab5da2c180f9baf16684f2cc7e11c0b
Filip Mařík <marikfi1@fel.cvut.cz>
2026-05-19 20:58:53 +0200

    Creating structures for paddles and ball

---
+++ ball.h
@@ -0,0 +1,12 @@
+#ifndef BALL_H
+#define BALL_H
+typedef struct{
+    double x;
+    double y;
+    double change_x;
+    double change_y;
+    double size;
+} ball_t;
+
+
+#endif
---
+++ paddle.h
@@ -0,0 +1,12 @@
+#ifndef PADDLE_H
+#define PADDLE_H
+#include <stdbool.h>
+
+typedef struct{
+    double y;
+    double height;
+    double width;
+    bool left;
+} paddle_t;
+
+#endif
