9656e51ecdd49b60433504f60eced3efd8c48054
Filip Mařík <marikfi1@fel.cvut.cz>
2026-05-20 17:45:30 +0200

    Add scoring and ball reset

--- ball.c
+++ ball.c
@@ -1,6 +1,6 @@
 #include "ball.h"
 
-void update_ball(ball_t* ball, paddle_t* left_p, paddle_t* right_p){
+state_t update_ball(ball_t* ball, paddle_t* left_p, paddle_t* right_p){
     ball->x += ball->change_x;
     ball->y += ball->change_y;
     if (ball->y - ball->size / 2 <= 0 || ball->y + ball->size / 2 >= 319){
@@ -11,6 +11,15 @@ void update_ball(ball_t* ball, paddle_t* left_p, paddle_t* right_p){
         
     } else if(collision_b_p(ball, right_p)){
        bounce(ball, right_p);
+    }
+    if (ball->x <= 0){
+        reset_ball(ball);
+        return SCORE_RIGHT;
+    } else if (ball->x >= 479){
+        reset_ball(ball);
+        return SCORE_LEFT;
+    } else {
+        return RALLY;
     }
 }
 
@@ -36,4 +45,11 @@ void bounce(ball_t* ball, paddle_t* paddle){
     if(ball->change_y <= -MAX_CHANGE_Y){
         ball->change_y = -MAX_CHANGE_Y;
     }
+}
+
+void reset_ball(ball_t* ball){
+    ball->x = CENTER_X;
+    ball->y = CENTER_Y;
+    ball->change_x = (rand() % 2 == 0) ? 3.0 : -3.0;
+    ball->change_y = ((rand() % 41) - 20) / 10.0; // -2.0 to 2.0
 }
--- ball.h
+++ ball.h
@@ -3,7 +3,11 @@
 
 #define KICK_MULT 0.15
 #define MAX_CHANGE_Y 5.0
+#define CENTER_X 240.0
+#define CENTER_Y 160.0
+#define INIT_SPEED 3.0
 
+#include <stdlib.h>
 #include "paddle.h"
 
 typedef struct{
@@ -14,8 +18,10 @@ typedef struct{
     double size;
 } ball_t;
 
-void update_ball(ball_t* ball);
+typedef enum{ RALLY, SCORE_LEFT, SCORE_RIGHT } state_t;
+state_t update_ball(ball_t* ball);
 bool collision_b_p(ball_t* ball, paddle_t* paddle);
 void bounce(ball_t* ball, paddle_t* paddle);
+void reset_ball(ball_t* ball);
 
 #endif
